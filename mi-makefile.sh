a0097e8ae6ed2434822e5d7e5d82d50e876b185b
Myriam Milha <myriam.milha@edu.univ-paris13.fr>
2026-01-14 17:51:54 +0100

    Add legacy build script

---
+++ mi-makefile.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Azul
+files=(
+    "Compilateur.class"
+    "Compilateur.java"
+    "CompilateurConstants.class"
+    "CompilateurConstants.java"
+    "CompilateurTokenManager.class"
+    "CompilateurTokenManager.java"
+    "ParseException.class"
+    "ParseException.java"
+    "SimpleCharStream.class"
+    "SimpleCharStream.java"
+    "Token.class"
+    "Token.java"
+    "TokenMgrError.class"
+    "TokenMgrError.java"
+)
+
+# test & delete
+for file in "${files[@]}"; do
+    if [ -f "$file" ]; then
+        rm "$file"
+        echo "$file deleted."
+    else
+      printf Clearing
+      sleep 0.2
+      printf .
+      sleep 0.2
+      printf .
+      sleep 0.2
+      printf .
+      sleep 0.2
+      printf .
+      clear
+        break
+    fi
+done
+# this "if" statement used to delete .java .class files (added files that we gonna not push everytime )
+if [ $# -eq 1 ]; then
+  rm AST/*.class
+  exit 1
+fi
+javac AST/*.java
+# program launching
+javacc Compilateur.jj
+javac *.java
+echo "---- Starting -----"
+java Compilateur
+./mi-makefile.sh 1 > t
+rm t
